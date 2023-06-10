4a09eb00635adcc8efee72fa65c064fd1c074f14
James Nolan Casey <34nolancasey34@gmail.com>
2023-06-10 19:51:06 -0400

    Added models to the three models.py.

--- apps/Order/models.py
+++ apps/Order/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.Product.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_order = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False,null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+    
+    
+class OrderType(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.ImageField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    
+class Adress(models.Model):
+    user = models.ForeignObject(User, on_delete=models.CASCADE)
+    street = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=25)
+    
+    
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+      
--- apps/Product/models.py
+++ apps/Product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TimeField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='products/images')
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TimeField()
+    image = models.ImageField(upload_to='category/images/')
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review_text = models.TextField()
